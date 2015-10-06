fdab921ef201ef9a690bd8232743583731f0036b
up201406458 <up201406458@62145ae9-d925-4efb-8371-7cfea75b35ae>
2015-10-06 15:34:22 +0000

    get_conf complete and tested

--- lab2/timer.c
+++ lab2/timer.c
@@ -1,46 +1,55 @@
 #include <minix/syslib.h>
 #include <minix/drivers.h>
 
+#include "timer.h"
+#include "i8254.h"
+
 int timer_set_square(unsigned long timer, unsigned long freq) {
 
-	return 1;
-}
+		return 1;
+	}
 
-int timer_subscribe_int(void ) {
+	int timer_subscribe_int(void) {
 
-	return 1;
-}
+		return 1;
+	}
 
-int timer_unsubscribe_int() {
+	int timer_unsubscribe_int() {
 
-	return 1;
-}
+		return 1;
+	}
 
-void timer_int_handler() {
+	void timer_int_handler() {
 
-}
+	}
 
-int timer_get_conf(unsigned long timer, unsigned char *st) {
-	
-	return 1;
-}
+	int timer_get_conf(unsigned long timer, unsigned char *st) {
+		char CMD = TIMER_RB_CMD | TIMER_RB_SEL(timer)|TIMER_RB_COUNT_;
+		if(sys_outb(TIMER_CTRL,CMD)==0){
+			if(sys_inb(TIMER_0+timer,st)==0){
+				return 0;
+			}
+			else return 1;
+		}
+		else return 1;
+	}
 
-int timer_display_conf(unsigned char conf) {
-	
-	return 1;
-}
+	int timer_display_conf(unsigned char conf) {
 
-int timer_test_square(unsigned long freq) {
-	
-	return 1;
-}
+		return 1;
+	}
 
-int timer_test_int(unsigned long time) {
-	
-	return 1;
-}
+	int timer_test_square(unsigned long freq) {
 
-int timer_test_config(unsigned long timer) {
-	
-	return 1;
-}
+		return 1;
+	}
+
+	int timer_test_int(unsigned long time) {
+
+		return 1;
+	}
+
+	int timer_test_config(unsigned long timer) {
+
+		return 1;
+	}
