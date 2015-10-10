c822e087fc147a21774ed615864f6714c792ed7f
up201403291 <up201403291@62145ae9-d925-4efb-8371-7cfea75b35ae>
2015-10-10 13:32:34 +0000

    display conf completed and tested

--- lab2/timer.c
+++ lab2/timer.c
@@ -6,6 +6,7 @@
 
 int timer_set_square(unsigned long timer, unsigned long freq) {
 
+
 		return 1;
 	}
 
@@ -35,9 +36,62 @@ int timer_set_square(unsigned long timer, unsigned long freq) {
 	}
 
 	int timer_display_conf(unsigned char conf) {
-
-		return 1;
-	}
+			int CM=conf & BIT(0);
+			if(CM==0)
+			{
+				printf("BCD\n");
+			}
+			else if(CM==1)
+			{
+				printf("Binar0y\n");
+			}
+			int OM=conf >> 1 & (BIT(0)|BIT(1)|BIT(2));
+			if(OM==0){
+				printf("Interrupt on Terminal Count\n");
+			}
+			else if(OM==1){
+						printf("Hardware Retriggerable One-shot\n");
+					}
+			else if(OM==2){
+				printf("Rate Gnerator\n");
+			}
+			else if(OM==3){
+				printf("Square Wave Mode\n");
+			}
+			else if(OM==4){
+						printf("Software Triggered Strobe\n");
+					}
+			else if(OM==5){
+				printf("Hardware Triggered Strobe(Retriggerable\n");
+			}
+			int TA=conf >> 4 & (BIT(0)|BIT(1));
+			if(TA==1){
+				printf("LSB");
+			}
+			else if(TA==2){
+				printf("MSB\n");
+			}
+			else if(TA==3){
+				printf("LSB followed by MSB\n");
+			}
+			int SC=conf >> 6 & BIT(0);
+			if(SC==0)
+			{
+				printf("Null Count=True\n");
+			}
+			else if(SC==1){
+				printf("Null Count=False\n");
+			}
+			int SC1=conf >> 7 & BIT(0);
+			if(SC1==0)
+					{
+						printf("Output=0\n");
+					}
+					else if(SC==1){
+						printf("Output=1\n");
+					}
+			return 0;
+		}
 
 	int timer_test_square(unsigned long freq) {
 
