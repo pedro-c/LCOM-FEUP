9454a06e1db3ecfcda8d950d71ca3dc5942233f6
up201403291 <up201403291@62145ae9-d925-4efb-8371-7cfea75b35ae>
2015-10-24 18:42:07 +0000

    kbd_test_leds almost completed

--- lab3/test3.c
+++ lab3/test3.c
@@ -53,8 +53,73 @@ int kbd_test_scan(unsigned short ass) {
 	}
 }
 
+
+int timer_subscribe_int(void) {
+
+	if ((sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hook_id)==OK))
+	{
+		sys_irqenable(&hook_id);//ativa as interrupcoes
+		return BIT(hook_id);
+	}
+	else return -1;
+
+}
+
+int timer_unsubscribe_int() {
+
+	if(sys_irqdisable(&hook_id)==OK) //desativar as interrupcoes
+	{
+		sys_irqrmpolicy(&hook_id); //Remove a policy do qual foi feito o disable
+		return 0;
+	}
+	else return 1;
+}
+
 int kbd_test_leds(unsigned short n, unsigned short *leds) {
-	/* To be completed */
+
+	//0 - scrlock
+	//1 - numlock
+	//2 - capslock
+
+	unsigned long irq_set;
+	int i=0;
+	int it = n*60; //numero de iterações vezes o tempo (60 segundos) de cada iteração
+	int r;
+	int ipc_status;
+	message msg;
+	unsigned long  numlk=0;
+	unsigned long  scrlk=0;
+	unsigned long  capslk=0;
+
+
+	irq_set=timer_subscribe_int();
+	if (kbd_subscribe(&hook_kbd) == -1) {
+				printf("Unable to subscribe!\n");
+				return -1;
+	}
+
+	while(i < it){
+
+		if(driver_receive(ANY,&msg,&ipc_status)!=0){
+			 printf("driver_receive failed with: %d", r);
+			 continue;
+		}
+
+		if(is_ipc_notify(ipc_status)) //recebe notificação
+		{
+			switch(_ENDPOINT_P(msg.m_source)){
+			case HARDWARE:
+				if(msg.NOTIFY_ARG & irq_set)
+
+			}
+		}
+
+
+
+	}
+
+
+
 }
 
 int kbd_test_timed_scan(unsigned short n) {
