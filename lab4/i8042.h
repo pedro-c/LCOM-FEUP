dc68275edd4bd440c8be3ceb769e9a44655d3ab4
up201406458 <up201406458@62145ae9-d925-4efb-8371-7cfea75b35ae>
2015-11-09 01:11:06 +0000

    adicinar mouse.h/.c e i8042.h.Comeco func test_packet

---
+++ lab4/i8042.h
@@ -0,0 +1,14 @@
+#ifndef __I8042_H
+#define __I8042_H
+
+
+#define BIT(n) (0x01<<(n))
+
+#define TIMER0_IRQ	        0
+#define NOTIFICATION_MOUSE  1
+#define NOTIFICATION_TIMER  0
+
+#define DELAY_US 20000
+
+#define MOUSE_IRQ           12
+#endif
