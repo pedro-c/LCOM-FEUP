db8ea27af5375de8f65f52d046be767dedf1a649
up201406458 <jpedrotm@users.noreply.github.com>
2015-11-10 02:07:47 +0000

    test_packet e test_async completas. test_packet alguns probl

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -1,101 +1,123 @@
 #include <minix/drivers.h>
 
+#include <minix/syslib.h>
+
 #include "test4.h"
 
 static int proc_args(int argc, char *argv[]);
-static unsigned long parse_ulong(char *str, int base);
+
+static unsigned short parse_ushort(char *str, int base);
+
 static void print_usage(char *argv[]);
 
 int main(int argc, char **argv) {
+
 	sef_startup();
-	sys_enable_iop(SELF);
 
 	if (argc == 1) {
+
 		print_usage(argv);
+
 		return 0;
+
 	} else {
+
 		proc_args(argc, argv);
+
 	}
+
 	return 0;
+
 }
 
-static void print_usage(char *argv[]) { //  quando chamado para testar mostra as funcoes que podem ser testadas
-	printf("Usage: one of the following:\n"
-			"\t service run %s -args \"test_packet <cnt>\" \n"
-			"\t service run %s -args \"test_async <idle_time>\" \n"
-			"\t service run %s -args \"test_config\" \n"
-			"\t service run %s -args \"test_gesture <length> <tolerance>\" \n",
-			argv[0], argv[0], argv[0], argv[0]);
+static void print_usage(char *argv[]) {
+
+	printf("\t service run %s -args \"test_packet <packets_count>\" \n"
+
+			"\t service run %s -args \"test_async <idle_time> \n"
+
+			"\t service run %s -args \"test_config \n"
+
+			"\t service run %s -args \"test_gesture <length> <tolerance> \n"
+
+	, argv[0], argv[0], argv[0], argv[0]);
+
 }
 
-static int proc_args(int argc, char *argv[]) { //chama a funcao correspondente
+static int proc_args(int argc, char *argv[]) {
 
-	unsigned short cnt, idle_time, tolerance;
+	unsigned short count, idle_time, tolerance;
+
 	short length;
 
-	/* check the function to test: if the first characters match, accept it */
 	if (strncmp(argv[1], "test_packet", strlen("test_packet")) == 0) {
+
 		if (argc != 3) {
-			printf("Mouse wrong no of arguments for test of test_packet()\n");
-			return 1;
-		}
-		if ((cnt = parse_ulong(argv[2], 10)) == USHRT_MAX)
-			return 1;
-		printf("Mouse::test_packet(%u)\n", (unsigned) cnt);
-		if (test_packet(cnt) == 0) {
-			return 0;
-		} else {
-			return 1;
-		}
-	} else if (strncmp(argv[1], "test_async", strlen("test_async")) == 0) {
-		if (argc != 3) {
+
 			printf(
-					"Mouse: wrong no of arguments for test of kbd_test_leds() \n");
+					"test4: wrong no of arguments for test of test_packet(unsigned short cnt)\n");
+
+			return 1;
+
+		}
+
+		if ((count = parse_ushort(argv[2], 10)) == USHRT_MAX)
+
+			return 1;
+
+		printf("test4::test_packet(%u)\n", count);
+
+		return test_packet(count);
+
+		///
+
+	} else if (strncmp(argv[1], "test_async", strlen("test_async")) == 0) {
+
+		if (argc != 3) {
+
+			printf("mouse: wrong no of arguments for test of test_async(unsigned short idle_time) \n");
 			return 1;
 		}
-		if ((idle_time = parse_ulong(argv[2], 16)) == ULONG_MAX)
+		if ((idle_time = parse_ushort(argv[2], 10)) == USHRT_MAX)
 			return 1;
-		printf("Mouse:: idle_time(%u)\n", (unsigned) idle_time);
+		printf("test4::test_async(%u)\n", idle_time);
 		return test_async(idle_time);
 	} else if (strncmp(argv[1], "test_config", strlen("test_config")) == 0) {
 		if (argc != 2) {
-			printf("Mouse: wrong no of arguments for test of vt_blank() \n");
+			printf("mouse: wrong no of arguments for test of test_config() \n");
 			return 1;
 		}
-		printf("Mouse:: test_config\n");
+		printf("mouse::test_config()\n");
 		return test_config();
-	}else if (strncmp(argv[1], "test_gesture", strlen("test_gesture")) == 0) {
-		if( argc != 4 ) {
-			printf("Mouse: wrong no of arguments for test of test_gesture() \n");
+	} else if (strncmp(argv[1], "test_gesture", strlen("test_gesture")) == 0) {
+		if (argc != 4) {
+			printf("mouse: wrong no of arguments for test of test_gesture(short length, unsigned short tolerance) \n");
 			return 1;
 		}
-		if((length = parse_ulong(argv[2], 16)) == ULONG_MAX )
-		return 1;
-		if((tolerance = parse_ulong(argv[3], 16)) == ULONG_MAX )
-		return 1;
-		printf("Mouse::test_gesture(%u,%u)\n",
-				(unsigned)length,(unsigned)tolerance);
-		return test_gesture(length,tolerance);
+		if ((length = parse_ushort(argv[2], 10)) == USHRT_MAX)
+			return 1;
+		if ((tolerance = parse_ushort(argv[2], 10)) == USHRT_MAX)
+			return 1;
+		printf("mouse::test_gesture(%d, %u)\n", length, tolerance);
+		return test_gesture(length, tolerance);
 	} else {
-		printf("Mouse: non valid function \"%s\" to test\n", argv[1]);
+		printf("mouse: non valid function \"%s\" to test\n", argv[1]);
 		return 1;
 	}
 }
 
-static unsigned long parse_ulong(char *str, int base) {
+unsigned short parse_ushort(char *str, int base) {
+
 	char *endptr;
-	unsigned long val;
-
+	unsigned short val;
 	val = strtoul(str, &endptr, base);
-
-	if ((errno == ERANGE && val == ULONG_MAX) || (errno != 0 && val == 0)) {
+	if ((errno == ERANGE && val == USHRT_MAX) || (errno != 0 && val == 0)) {
 		perror("strtol");
-		return ULONG_MAX;
+		return USHRT_MAX;
 	}
-
 	if (endptr == str) {
-		printf("Mouse: parse_ulong: no digits were found in %s \n", str);
-		return ULONG_MAX;
+		printf("mouse: parse_ushort: no digits were found in %s \n", str);
+		return USHRT_MAX;
 	}
 	return val;
 }
--- lab4/mouse.h
+++ lab4/mouse.h
@@ -12,6 +12,10 @@ static int hook_timer=NOTIFICATION_TIMER;
 
 int timer_subscribe_int(void);
 int timer_unsubscribe_int();
-
+int mouse_subscribe();
+int mouse_unsubscribe();
+int mouse_write_command(char port, unsigned char cmd);
+int mouse_read();
+void mouse_print(char packet[]);
 
 #endif
