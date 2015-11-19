00d1a757bc44a6fa521353b3c98e706824deed9d
up201406458 <up201406458@62145ae9-d925-4efb-8371-7cfea75b35ae>
2015-11-19 22:33:31 +0000

    test_square corrigida

---
+++ lab5/keyboard.h
@@ -0,0 +1,29 @@
+#ifndef __KEYBOARD_H
+#define __KEYBOARD_H
+
+#include <minix/sysutil.h>
+#include <minix/syslib.h>
+#include <minix/drivers.h>
+#include "i8254.h"
+
+static int hook_kbd=NOTIFICATION_KBD;
+static int hook_timer=NOTIFICATION_TIMER;
+
+int kbd_subscribe(int *hook_kbd);
+
+int kbd_unsubscribe(int *hook_kbd);
+
+int kbd_code_scan (unsigned char *codigo);
+
+void print_code(unsigned char codigo);
+
+int sendInst(unsigned long reg, unsigned long cmd);
+
+int set_leds(unsigned long cmd);
+
+int wait_for_ESC();
+
+
+
+
+#endif
