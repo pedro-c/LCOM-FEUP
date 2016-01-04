07bee3302b14e0d19611136250b4c65ad405dad2
up201403291 <pedro-c@users.noreply.github.com>
2016-01-04 01:29:26 +0000

    half of the code commented

--- proj/source/Button.h
+++ proj/source/Button.h
@@ -1,8 +1,30 @@
 #pragma once
 
+
+/** @defgroup Button Button
+ * @{
+ * Struct containing the boundaries of the button
+ */
 typedef struct{
 	int x1,y1,x2,y2;
 }Box;
 
+/**
+ * @brief creates a new button (boundaries)
+ *
+ * @param x coordinate of left top corner
+ * @param y coordinate of left top corner
+ * @param x coordinate of right bottom corner
+ * @param y coordinate of left bottom corner
+ * @return Non NULL pointer to the new box
+ */
 Box* newBox(int x1, int y1, int x2, int y2);
+
+/**
+ * @brief deletes the box
+ *
+ * @param box to be deleted
+ */
 void deleteBox(Box* box);
+
+/**@}*/
--- proj/source/FinalState.h
+++ proj/source/FinalState.h
@@ -6,6 +6,10 @@
 #include "Bitmap.h"
 #include "fonts.h"
 
+/** @defgroup FinalState FinalState
+ * @{
+ * Struct containing all the information of the final state
+ */
 typedef struct{
 
 	Fonts* f;
@@ -31,15 +35,63 @@ typedef struct{
 
 }FinalState;
 
+/**
+ * @brief Creates a new final state
+ *
+ * @return Non NULL pointer final state
+ */
+
 FinalState* newFinalState();
+
+/**
+ * @brief updates the final state
+ *
+ * @param state to update
+ * @param keyboard scancode
+ * @param score
+ * @param variable containing the final score of the player
+ * @return int telling if the player wants to close the game
+ */
 int updateFinalState(FinalState* state, unsigned long scancode, int score, int scoreAux);
+
+/**
+ * @brief reads from a file the highest scores achivied before
+ *
+ * @param state to update
+ */
 void readHighScores(FinalState* state);
+/**
+ * @brief prints the highest scores
+ *
+ * @param state to update
+ */
 void printHighScores(FinalState* state);
+/**
+ * @brief defines a new high score and saves it to a text file
+ *
+ * @param state to update
+ */
 void newHighScore(FinalState* state);
+
+/**
+ * @brief verifies if the state there is a need to change to another state
+ *
+ * @param state to update
+ */
 int verifyFinalStateChange(FinalState* state);
+/**
+ * @brief draws the state
+ *
+ * @param state to update
+ */
 void drawFinalMenu(FinalState* state);
+/**
+ * @brief deletes the state
+ *
+ * @param state to be deleted
+ */
 void deleteFinalState(FinalState* state);
 
-
+/**@}*/
 
 #endif
